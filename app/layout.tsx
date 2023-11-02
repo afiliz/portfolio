b13e3cda610fd83ad494024d8df04ee0017d6cef
Adam Filiz <filizadam@gmail.com>
2023-11-01 23:48:52 -0600

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+            <Analytics />
         </ActiveSectionContextProvider>
-        <Analytics />
       </body>
     </html>
   )

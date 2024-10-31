348ad41d434bb810b88e9307e213970283efec52
Adam Filiz <filizadam@gmail.com>
2024-10-31 00:17:18 -0700

    Fix swiper being too large on mobile

--- components/project.tsx
+++ components/project.tsx
@@ -31,7 +31,7 @@ export default function Project({title, url, description, tags, imageUrl}:
       className="group mb-14 sm:mb-8 last:mb-0"
     >
       <section
-        className="relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:max-w-[47rem] sm:pr-8 max-h-[22rem] rounded-lg hover:bg-gray-200 transition">
+        className="relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:max-w-[47rem] sm:pr-8 max-h-[26rem] md:max-h-[23rem] rounded-lg hover:bg-gray-200 transition">
         <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[60%] flex flex-col h-full sm:group-even:ml-[18rem]'>
           <h3 className='text-2xl font-semibold'><a className="hover:underline" target="_blank">{title}</a></h3>
           <p className='mt-2 mb-2 leading-relaxed text-gray-700'>{description}</p>
